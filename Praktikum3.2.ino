302770a3ed6350ddfb0297191a6f5c56ad1b9141
Aprijal <iskandaraprijal@gmail.com>
2016-09-13 07:48:01 +0700

    Add files via upload

---
+++ Praktikum3.2.ino
@@ -0,0 +1,41 @@
+void setup()// put your setup code here, to run once:
+{DDRB=0b1111; 
+DDRC=0b0000; 
+PORTC=0b1111; 
+
+}
+
+void loop() 
+{
+   if(digitalRead(A0)==LOW)     
+   {  
+    forward();
+   }
+   if(digitalRead(A1)==LOW) 
+     { 
+       reverse();
+     } 
+    
+   if(digitalRead(A2)==LOW) 
+     { 
+      reset(); 
+     }
+     
+}
+
+void reset()
+{
+  PORTB =0;
+}
+
+void reverse()
+{
+   for(int b=11; b>7; b--)
+    { 
+      digitalWrite(b,HIGH);
+      delay(1000);
+      digitalWrite(b,LOW); 
+    }
+}
+
+
